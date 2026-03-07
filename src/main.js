eebd72b427672d13c667d30504cce46c5a07abf2
Eman Swerky <eman.mahmoud.swerky@gmail.com>
2026-03-06 19:43:31 -0800

    install primeVue

--- src/main.js
+++ src/main.js
@@ -1,17 +1,29 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import './assets/main.css'
 import App from './App.vue'
+import router from './router'
 
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
-
 import 'primeicons/primeicons.css'
 
 const app = createApp(App)
+import Chart from 'primevue/chart'
+app.component('Chart', Chart)
+
+import { Chart as ChartJS } from 'chart.js/auto'
+void ChartJS
+
+app.use(createPinia())
+app.use(router)
 
 app.use(PrimeVue, {
-  theme: {
-    preset: Aura,
+  theme: { preset: Aura },
+  pt: {
+    apply: true,
   },
+  ptOptions: { mergeSections: true, mergeProps: true },
 })
 
 app.mount('#app')
