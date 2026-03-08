295a1b20827e993a52e4c0b04b2376a34c8876eb
Eman Swerky <eman.mahmoud.swerky@gmail.com>
2026-03-08 13:37:55 -0700

    configure router for dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const routes = [
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: () => import('../pages/Dashboard.vue'),
+  },
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
