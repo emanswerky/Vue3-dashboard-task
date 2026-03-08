5055a8d1283b3c5e2c86b85d0c624bd321cf0d0b
Eman Swerky <eman.mahmoud.swerky@gmail.com>
2026-03-08 14:59:52 -0700

    create KpiSection component

---
+++ src/components/kpisSection.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 2xl:grid-cols-7 gap-3">
+    <KpiCard v-for="(item, index) in kpis" :key="index" v-bind="item" />
+  </div>
+</template>
+<script setup>
+import { useKpiCard } from '/src/composables/useKpiCard'
+import KpiCard from '/src/components/KpiCard.vue'
+
+const { kpis } = useKpiCard()
+</script>
