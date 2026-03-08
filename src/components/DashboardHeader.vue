783f47c6bd1272e1a676ca20b15986b9c77649ab
Eman Swerky <eman.mahmoud.swerky@gmail.com>
2026-03-08 15:15:00 -0700

    create header section

---
+++ src/components/DashboardHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="select-none">
+    <div class="sm:text-[20px] text-[18px] text-slate-800">
+      The <span class="text-slate-800 font-extrabold">Essentials.</span>
+    </div>
+
+    <div class="break-words my-4 sm:text-[14px] text-[12px] font-medium text-[#70b0e7]">
+      Here's how your business is performing today.
+    </div>
+  </div>
+</template>
