62955fb40a1f06bff0ada639818e1651e89ce6b6
Eman Swerky <eman.mahmoud.swerky@gmail.com>
2026-03-08 14:57:46 -0700

    create DashboardLayout component

---
+++ src/layouts/DashboardLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="md:m-3 m-0 md:ml-0 bg-slate-100 flex">
+    <Sidebar :open="sidebarOpen" />
+
+    <div class="flex flex-col lg:pl-[84px] sm:pl-[70px] p-[0px] w-full">
+      <div class="">
+        <TopNavbar @toggle-sidebar="toggleSidebar" />
+      </div>
+      <main class="sm:p-4 p-2">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+import Sidebar from '@/components/Sidebar.vue'
+import TopNavbar from '@/components/TopNav.vue'
+const sidebarOpen = ref(false)
+
+const toggleSidebar = () => {
+  sidebarOpen.value = !sidebarOpen.value
+}
+</script>
